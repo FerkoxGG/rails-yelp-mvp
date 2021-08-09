eaaf3746fa37a54d4b6255432018e1b6933d40d0
Fernando Antonio Gonzalez Gaete <fernandogonzalezgaete@gmail.com>
2021-08-09 17:49:52 -0400

    avance de controladores

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  belongs_to :restaurant
+  validates :rating, inclusion: 0..5
+  validates :rating, numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    resources :reviews, only: [:new, :create] do
+    resources :reviews, only: %i[new create] do
       collection do
         get :top
       end
     end
   end
-  resources :reviews, only: [ :destroy ]
-  resources :comments, only: [:show, :edit, :update, :destroy]
+  resources :reviews, only: [:destroy]
+  resources :comments, only: %i[show edit update destroy]
 end
